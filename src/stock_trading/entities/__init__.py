35b02bc9462fa3f1131771d93e5e26471d054a4d
KevinRabbe <rabbekevin@icloud.com>
2026-08-11 17:53:59 +0200

    export canonical company entities

---
+++ src/stock_trading/entities/__init__.py
@@ -0,0 +1,13 @@
+from .company import (
+    CompanyIdentity,
+    CompanyRegistry,
+    company_id_from_sec_cik,
+    normalize_sec_cik,
+)
+
+__all__ = [
+    "CompanyIdentity",
+    "CompanyRegistry",
+    "company_id_from_sec_cik",
+    "normalize_sec_cik",
+]

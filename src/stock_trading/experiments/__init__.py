3f8cf1cf15172c0a861d435becdf7b4b86856d9c
KevinRabbe <rabbekevin@icloud.com>
2026-08-12 23:47:17 +0200

    export historical experiment runner

---
+++ src/stock_trading/experiments/__init__.py
@@ -0,0 +1,11 @@
+from .lightgbm import (
+    HistoricalExperimentConfig,
+    HistoricalExperimentResult,
+    run_historical_experiment,
+)
+
+__all__ = [
+    "HistoricalExperimentConfig",
+    "HistoricalExperimentResult",
+    "run_historical_experiment",
+]

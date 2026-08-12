f7f1ff92565368d2a5ef1aee6db4d28c8f41d1b2
KevinRabbe <rabbekevin@icloud.com>
2026-08-12 23:57:41 +0200

    export data preparation and enrichment workflows

--- src/stock_trading/experiments/__init__.py
+++ src/stock_trading/experiments/__init__.py
@@ -1,11 +1,29 @@
+from .enrich import (
+    LdaEnrichmentConfig,
+    LdaEnrichmentResult,
+    enrich_lda_and_qwen,
+)
 from .lightgbm import (
     HistoricalExperimentConfig,
     HistoricalExperimentResult,
     run_historical_experiment,
 )
+from .prepare import (
+    BENCHMARK_SPY_COMPANY_ID,
+    SecMarketPopulationConfig,
+    SecMarketPopulationResult,
+    populate_sec_and_market,
+)
 
 __all__ = [
+    "BENCHMARK_SPY_COMPANY_ID",
     "HistoricalExperimentConfig",
     "HistoricalExperimentResult",
+    "LdaEnrichmentConfig",
+    "LdaEnrichmentResult",
+    "SecMarketPopulationConfig",
+    "SecMarketPopulationResult",
+    "enrich_lda_and_qwen",
+    "populate_sec_and_market",
     "run_historical_experiment",
 ]

08bbc6b229574ca5468d201d3c30d8b67e8d00a1
KevinRabbe <rabbekevin@icloud.com>
2026-08-12 23:22:36 +0200

    export semantic extraction components

---
+++ src/stock_trading/extraction/__init__.py
@@ -0,0 +1,9 @@
+from .qwen import FileSemanticCache, QwenSemanticExtractor
+from .semantic import ALLOWED_TOPICS, SemanticResult
+
+__all__ = [
+    "ALLOWED_TOPICS",
+    "FileSemanticCache",
+    "QwenSemanticExtractor",
+    "SemanticResult",
+]

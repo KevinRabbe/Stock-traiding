d8980c72990238e721438227cdd7a40ad1355181
KevinRabbe <rabbekevin@icloud.com>
2026-08-12 23:38:31 +0200

    export deterministic backtest components

---
+++ src/stock_trading/backtest/__init__.py
@@ -0,0 +1,15 @@
+from .portfolio import (
+    BacktestConfig,
+    BacktestResult,
+    FixedAllocationBacktester,
+    ScoredCandidate,
+    TradeRecord,
+)
+
+__all__ = [
+    "BacktestConfig",
+    "BacktestResult",
+    "FixedAllocationBacktester",
+    "ScoredCandidate",
+    "TradeRecord",
+]

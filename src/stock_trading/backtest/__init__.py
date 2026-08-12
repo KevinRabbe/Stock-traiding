b3f568bb83601cb6b4fc9814bc2c73eb224a15aa
KevinRabbe <rabbekevin@icloud.com>
2026-08-12 23:42:08 +0200

    export backtest reporting tools

--- src/stock_trading/backtest/__init__.py
+++ src/stock_trading/backtest/__init__.py
@@ -5,11 +5,23 @@ from .portfolio import (
     ScoredCandidate,
     TradeRecord,
 )
+from .reporting import (
+    ScoreBucketResult,
+    WalkForwardSummary,
+    evaluate_score_buckets,
+    profit_without_best_trades,
+    summarize_walk_forward,
+)
 
 __all__ = [
     "BacktestConfig",
     "BacktestResult",
     "FixedAllocationBacktester",
+    "ScoreBucketResult",
     "ScoredCandidate",
     "TradeRecord",
+    "WalkForwardSummary",
+    "evaluate_score_buckets",
+    "profit_without_best_trades",
+    "summarize_walk_forward",
 ]

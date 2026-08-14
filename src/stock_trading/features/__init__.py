ad433ad6542fbf1c43d6f70244d3e8ab0b1ed7cd
KevinRabbe <rabbekevin@icloud.com>
2026-08-15 01:18:26 +0200

    Export temporal company event index

--- src/stock_trading/features/__init__.py
+++ src/stock_trading/features/__init__.py
@@ -5,13 +5,16 @@ from .alternative import (
     build_lobbying_features,
 )
 from .congress import build_congress_features
+from .event_index import CompanyEventIndex, ensure_company_event_index
 from .insider import build_insider_features
 
 __all__ = [
+    "CompanyEventIndex",
     "build_alternative_features",
     "build_congress_features",
     "build_contract_features",
     "build_cross_source_features",
     "build_insider_features",
     "build_lobbying_features",
+    "ensure_company_event_index",
 ]
